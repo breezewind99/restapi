8070c120020e8bb8b326fe7d2df580bf1b7fd315
breeze <breezewind1209@gmail.com>
2022-08-19 20:09:25 +0900

    기본 작업 완료 FFT 이미지 관련 작업 남음

--- src/main/java/com/cnettech/restapi/common/WaveProcess.java
+++ src/main/java/com/cnettech/restapi/common/WaveProcess.java
@@ -2,15 +2,58 @@ package com.cnettech.restapi.common;
 
 import com.cnettech.restapi.common.jna.SslEncryption;
 import com.sun.jna.Native;
+import ie.corballis.sox.SoXEffect;
+import ie.corballis.sox.SoXEncoding;
+import ie.corballis.sox.Sox;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 public class WaveProcess {
 
-    public void WaveDecryption(String Source_File, String Target_File) {
+    public static void WaveDecryption(String Source_File, String Target_File) {
         int ResultLib = 0;
+//        String sValue = "02B317631220B55742F5712B5E025B0ED3027CD58E423AFBF8ABEAAA2716BCE1";
+        String skey = "7912A53404EA2165E842A0228159E89557F0A8C1717F88CEC04B7F42F077DDE6";
         String sValue = "02B317631220B55742F5712B5E025B0ED3027CD58E423AFBF8ABEAAA2716BCE1";
         final SslEncryption eSslEncryption= (SslEncryption) Native.load("cnetssl", SslEncryption.class);
-        ResultLib = eSslEncryption.AES_Init_SSL("", 16, "25800450", 1);
-        ResultLib = eSslEncryption.AES_Dec_SSL_V2(Source_File, Target_File,sValue, sValue.length(), 1, 0);
+        ResultLib = eSslEncryption.AES_Init_SSL(skey, skey.length(), "25800450", 1);
+        ResultLib = eSslEncryption.AES_Dec_SSL_V2(Source_File, Target_File, sValue, sValue.length(), 1, 0);
+        System.out.println("OUT" + ResultLib);
     }
-    
+
+    public static void WaveConvert(String Source_File, String Target_File) {
+        System.out.println("Convert Source : " + Source_File + ", Target : " + Target_File);
+        // MP3로 요청이 올경우 작업처리
+        String source = Source_File.replace(".mp3",".wav");
+        String target = Target_File.replace(".mp3",".wav");
+
+        Sox sox_pcm = new Sox("/usr/local/bin/sox");
+        Sox sox_mp3 = new Sox("/usr/local/bin/sox");
+        try{
+            System.out.println("Target : " + target.replace(".wav",".pcm.wav"));
+            // Gsm To Pcm
+            sox_pcm
+                    .sampleRate(8000)
+                    .inputFile(source)
+                    .encoding(SoXEncoding.SIGNED_INTEGER)
+                    .bits(16)
+                    .outputFile(target.replace(".wav",".pcm.wav"))
+                    .execute();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            System.out.println("Target : " + target.replace(".wav",".mp3"));
+            // Gsm To MP3
+            sox_mp3
+                .sampleRate(8000)
+                .inputFile(source)
+                .argument("-t", "mp3")
+                .outputFile(target.replace(".wav",".mp3"))
+                .execute();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/cnettech/restapi/controller/Media.java
+++ src/main/java/com/cnettech/restapi/controller/Media.java
@@ -1,5 +1,8 @@
 package com.cnettech.restapi.controller;
 
+import com.cnet.CnetCrypto.CNCrypto;
+import com.cnettech.restapi.common.WaveProcess;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -20,17 +23,38 @@ import java.nio.file.Paths;
 @RequestMapping("/media")
 public class Media {
 
+    @Value("${MEDIA.Storage_Default}")
+    private String Storage_Default;
     @GetMapping("/wave")
-    public ResponseEntity<InputStreamResource> Wave(@RequestParam(defaultValue = "test") String ref) throws IOException {
-//        localhost:8080/?fName=test.mp3
-        System.out.println("fName = " + ref);
+    public ResponseEntity<InputStreamResource> Wave(@RequestParam(defaultValue = "test") String refer) throws IOException {
+//        localhost:8080/media/wave/?ref=test.mp3
+
+        System.out.println("ref = " + refer);
+        String tmp = "";
+        try {
+            CNCrypto aes = new CNCrypto("AES","!@CNET#$");
+            tmp = aes.Decrypt(refer);
+            System.out.println("Convert Valuue : " + tmp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String temp[] = tmp.split("\\|");
+
         // 파일 경로
-        String file_Path = "E:\\temp\\";
-        String path = file_Path + File.separator + ref;
-        // 파일 존재 유무
-        boolean fExist = Files.exists(Paths.get(path));
-        if (fExist) {
-            File file = new File(path);
+        String filepath = temp[2];
+        String path = (Storage_Default + File.separator + filepath).replace("/",File.separator).replace(File.separator + File.separator,File.separator);
+        System.out.println("path = " + path);
+        String TargetFile = "/data/temp/" + Paths.get(path).getFileName().toString();
+        boolean OriginExist = Files.exists(Paths.get(path));
+        boolean TargetExist = Files.exists(Paths.get(TargetFile));
+        if (!TargetExist) {
+            // 임시 폴더에 대상 파일이 없는경우 컨버팅 한다.
+            WaveProcess.WaveConvert(path, TargetFile);
+        }
+
+        try {
+            File file = new File(TargetFile);
             String fileName = file.getName();
             // 파일 확장자
             String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
@@ -53,7 +77,103 @@ public class Media {
                     //.contentType(MediaType.parseMediaType("application/octet-stream"))
                     .contentType(MediaType.parseMediaType("audio/mpeg"))
                     .body(resource3);
+        } catch ( Exception e) {
+            e.printStackTrace();
+            return null;
         }
-        return null;
+    }
+
+    @GetMapping("/fft")
+    public ResponseEntity<InputStreamResource> Fft(@RequestParam(defaultValue = "test") String refer) throws IOException {
+//        localhost:8080/media/wave/?ref=test.mp3
+
+        System.out.println("ref = " + refer);
+        String tmp = "";
+        try {
+            CNCrypto aes = new CNCrypto("AES","!@CNET#$");
+            tmp = aes.Decrypt(refer);
+            System.out.println("Convert Valuue : " + tmp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String temp[] = tmp.split("\\|");
+
+        // 파일 경로
+        String filepath = temp[2];
+        String path = (Storage_Default + File.separator + filepath).replace("/",File.separator).replace(File.separator + File.separator,File.separator);
+        System.out.println("path = " + path);
+        String TargetFile = "/data/temp/" + Paths.get(path).getFileName().toString();
+        boolean OriginExist = Files.exists(Paths.get(path));
+        boolean TargetExist = Files.exists(Paths.get(TargetFile));
+        if (!TargetExist) {
+            // 임시 폴더에 대상 파일이 없는경우 컨버팅 한다.
+            WaveProcess.WaveConvert(path, TargetFile);
+        }
+
+        try {
+            File file = new File(TargetFile);
+            String fileName = file.getName();
+            // 파일 확장자
+            String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
+            HttpHeaders header = new HttpHeaders();
+            Path fPath = Paths.get(file.getAbsolutePath());
+
+            //header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+            header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            header.add("Pragma", "no-cache");
+            header.add("Expires", "0");
+
+            // 대용량일 경우 resource3을 사용해야함
+//	        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(fPath ));
+//	        Resource resouce2 = resourceLoader.getResource(path);
+            InputStreamResource resource3 = new InputStreamResource(new FileInputStream(file));
+
+            return ResponseEntity.ok()
+                    .headers(header)
+                    .contentLength(file.length())
+                    //.contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .contentType(MediaType.parseMediaType("audio/mpeg"))
+                    .body(resource3);
+        } catch ( Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @GetMapping("/decrypt")
+    public void Decrypt(@RequestParam(defaultValue = "test") String ref) throws IOException {
+//        localhost:8080/media/wave/?ref=test.mp3
+        System.out.println("ref = " + ref);
+        String temp[] = ref.split("\\|");
+        // 파일 경로
+        String filepath = temp[2];
+        String path = (Storage_Default + File.separator + filepath).replace("/",File.separator).replace(File.separator + File.separator,File.separator);
+        System.out.println("path = " + path);
+        String path_Dec = path + ".dec";
+        WaveProcess.WaveDecryption(path, path_Dec);
+    }
+
+    @GetMapping("/convert")
+    public void Convert(@RequestParam(defaultValue = "test") String ref) throws IOException {
+//        localhost:8080/media/wave/?ref=test.mp3
+        System.out.println("ref = " + ref);
+        String temp[] = ref.split("\\|");
+        // 파일 경로
+        String filepath = temp[2];
+        String path = (Storage_Default + File.separator + filepath).replace("/",File.separator).replace(File.separator + File.separator,File.separator);
+        System.out.println("path = " + path);
+//        WaveProcess.WaveConvert(path);
+    }
+
+    @GetMapping("/decryptstring")
+    public void decryptstring(@RequestParam(defaultValue = "test") String ref) {
+        try {
+            CNCrypto aes = new CNCrypto("AES","!@CNET#$");
+            String returnValue = aes.Decrypt(ref);
+            System.out.println("Convert Valuue : " + returnValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
