c3a1189cd602088d90bf2953d8d46d38afdb29a6
breeze <breezewind99@cnettech.co.kr>
2022-08-22 10:46:41 +0900

    암호화 관련 작업 진행

--- src/main/java/com/cnettech/restapi/common/WaveProcess.java
+++ src/main/java/com/cnettech/restapi/common/WaveProcess.java
@@ -2,26 +2,19 @@ package com.cnettech.restapi.common;
 
 import com.cnettech.restapi.common.jna.SslEncryption;
 import com.sun.jna.Native;
-import ie.corballis.sox.SoXEffect;
 import ie.corballis.sox.SoXEncoding;
 import ie.corballis.sox.Sox;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
-import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
+@Slf4j
 public class WaveProcess {
 
     public String Sox_Path;
@@ -32,13 +25,70 @@ public class WaveProcess {
 
     public void WaveDecryption(String Source_File, String Target_File) {
         int ResultLib = 0;
+
+        String[] sValue = new String[] {
+                "02B317631220B55742F5712B5E025B0ED3027CD58E423AFBF8ABEAAA2716BCE1",
+                "B3D00127CB4A695935D34FEB30EC76699FAFDA58A3E9B966254C5E9533090854",
+                "C614C96E7B2BB970BF9DE92A1A0FD98203339870B4E741C04528FA5A1FFFBAC5",
+                "C23896E69FED3B1DC03E989C6013E9D14D1B3DE27D0397001783986D05C5A9D5",
+                "39A2E9B3F8BF3A0452CA8072173EAAF0021F81A83247944E6C0B7BDA3ABC1294",
+                "8029C5982387CD5986FEB3210C73E37F6EB8E133BE2BDDA1A0C20DCBB511FC80",
+                "A1F17FDDCE72385C1E37604F037B9FFD0487656C756247693B5A0AF326E4F29A",
+                "8208237F66FE73DB3E368429053E47AF8F3B05B0BB837195C364014C6D2B8152",
+                "E4230B9DECE5879D7304BFC6F8F85314BB6B9226E504F95A2BC3D2E3363838A1",
+                "35FD24B172BC38B1B9278D1A89ADD04F913CD627F6C2DEB94900EA2218BC1933"
+        };
 //        String sValue = "02B317631220B55742F5712B5E025B0ED3027CD58E423AFBF8ABEAAA2716BCE1";
-        String skey = "7912A53404EA2165E842A0228159E89557F0A8C1717F88CEC04B7F42F077DDE6";
-        String sValue = "02B317631220B55742F5712B5E025B0ED3027CD58E423AFBF8ABEAAA2716BCE1";
-        final SslEncryption eSslEncryption= (SslEncryption) Native.load("cnetssl", SslEncryption.class);
-        ResultLib = eSslEncryption.AES_Init_SSL(skey, skey.length(), "25800450", 1);
-        ResultLib = eSslEncryption.AES_Dec_SSL_V2(Source_File, Target_File, sValue, sValue.length(), 1, 0);
-        System.out.println("OUT" + ResultLib);
+//        String sValue = "B3D00127CB4A695935D34FEB30EC76699FAFDA58A3E9B966254C5E9533090854";
+//        String sValue = "C614C96E7B2BB970BF9DE92A1A0FD98203339870B4E741C04528FA5A1FFFBAC5";
+//        String sValue = "C23896E69FED3B1DC03E989C6013E9D14D1B3DE27D0397001783986D05C5A9D5";
+//        String sValue = "39A2E9B3F8BF3A0452CA8072173EAAF0021F81A83247944E6C0B7BDA3ABC1294";
+//        String sValue = "8029C5982387CD5986FEB3210C73E37F6EB8E133BE2BDDA1A0C20DCBB511FC80";
+//        String sValue = "A1F17FDDCE72385C1E37604F037B9FFD0487656C756247693B5A0AF326E4F29A";
+//        String sValue = "8208237F66FE73DB3E368429053E47AF8F3B05B0BB837195C364014C6D2B8152";
+//        String sValue = "E4230B9DECE5879D7304BFC6F8F85314BB6B9226E504F95A2BC3D2E3363838A1";
+//        String sValue = "35FD24B172BC38B1B9278D1A89ADD04F913CD627F6C2DEB94900EA2218BC1933";
+
+        final SslEncryption eSslEncryption= Native.load("cnetssl", SslEncryption.class);
+        ResultLib = eSslEncryption.AES_Init_SSL("", 0, "25800450", 1);
+        log.info("AES_Init_SSL Result : " + ResultLib);
+        ResultLib = eSslEncryption.AES_Dec_SSL_V2(Source_File, Target_File, sValue[2], sValue[2].length(), 1, 0);
+        log.info("AES_Dec_SSL_V2 File : " + Source_File + ", Result : " + ResultLib);
+    }
+
+    public boolean CheckRiff(String filename) {
+        try {
+
+            byte[] buffer = new byte[4];
+            InputStream is = new FileInputStream(filename);
+            is.read(buffer);
+            String coverted = new String(buffer);
+            is.close();
+            log.info("Check File RIFF : " + filename + ", Result : " + coverted);
+            if ("RIFF".equals(coverted)) {
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public String KeyValue(String filename) {
+        try {
+            String str = filename.substring(0,14);
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+            Date date = format.parse(str);
+            Calendar calendar = new GregorianCalendar();
+            calendar.setTime(date);
+            int year = calendar.get(Calendar.YEAR);
+            log.info("KeyValue : " + filename + ", Result : " + filename.substring(3, 4));
+            return filename.substring(3, 4);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
     public void WaveConvert(String Source_File, String Target_File) {
--- src/test/java/com/cnettech/restapi/RestapiApplicationTests.java
+++ src/test/java/com/cnettech/restapi/RestapiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.cnettech.restapi;
 
+import com.cnettech.restapi.common.WaveProcess;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,12 @@ class RestapiApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void readtest() {
+		WaveProcess waveProcess = new WaveProcess("test",1,1);
+		waveProcess.CheckRiff("E:\\Project_Java\\recorddata\\20220820\\02\\20220820024707_19906.tx");
+		waveProcess.CheckRiff("E:\\Project_Java\\recorddata\\20220820\\02\\20220820024707_19906.wav");
+		waveProcess.KeyValue("20220820024707_19906.wav");
+		waveProcess.KeyValue("20210101010101_19906.wav");
+	}
 }
